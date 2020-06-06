852c58c40ea7aca1ee04371c68f3c5f9d3a8871e
Алексей Буткеев <abutkeev@MacBook-Pro.local>
2020-06-06 16:31:15 +0300

    video buttons

--- src/components/Channel/VideoList/VideoCard/Buttons.js
+++ src/components/Channel/VideoList/VideoCard/Buttons.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Link as RouterLink } from 'react-router-dom';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+
+import CopyButton from '../../../CopyButton';
 
 function Buttons(props) {
+    const url = 'https://www.youtube.com/watch?v=' + props.id
     return (
         <Box>
-            
+            <Button startIcon={<EqualizerIcon />} component={RouterLink} to={'/video/' + props.id + '/stats'}>Статистика</Button>
+            <Tooltip title='Открыть на YouTube'>
+                <IconButton target='_blank' rel='noopener' href={url}><YouTubeIcon /></IconButton>
+            </Tooltip>
+            <CopyButton text={url} />
         </Box>
     )
 }
