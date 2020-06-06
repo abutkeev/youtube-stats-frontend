2aee918cb259340ba674df17eadac2e169ad0189
Алексей Буткеев <abutkeev@MacBook-Pro.local>
2020-06-06 21:57:58 +0300

    add home button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
 
 export const AppHeader = () => {
     return (
         <React.Fragment>
             <AppBar position='fixed'>
                 <Toolbar>
+                    <IconButton edge='start' component={RouterLink} to='/'><HomeIcon/></IconButton>
                     <Typography variant='h5'>
                         Header
                     </Typography>
