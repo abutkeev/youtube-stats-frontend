0c492389dec7bbe6d314fd3b0114338611f5fa87
Алексей Буткеев <abutkeev@MacBook-Pro.local>
2020-06-06 21:22:34 +0300

    change icon for youtube link

--- src/components/Channel/VideoList/VideoCard/Buttons.js
+++ src/components/Channel/VideoList/VideoCard/Buttons.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import { Link as RouterLink } from 'react-router-dom';
-import YouTubeIcon from '@material-ui/icons/YouTube';
+import LinkIcon from '@material-ui/icons/Link';
 import EqualizerIcon from '@material-ui/icons/Equalizer';
 
 import CopyButton from '../../../CopyButton';
@@ -15,7 +15,7 @@ function Buttons(props) {
         <Box>
             <Button startIcon={<EqualizerIcon />} component={RouterLink} to={'/video/' + props.id + '/stats'}>Статистика</Button>
             <Tooltip title='Открыть на YouTube'>
-                <IconButton target='_blank' rel='noopener' href={url}><YouTubeIcon /></IconButton>
+                <IconButton target='_blank' rel='noopener' href={url}><LinkIcon /></IconButton>
             </Tooltip>
             <CopyButton text={url} />
         </Box>
