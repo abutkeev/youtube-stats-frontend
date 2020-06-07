4fe122bcf8ad4e5f2a55018d7f4a6d1fbaff0b33
Алексей Буткеев <abutkeev@MacBook-Pro.local>
2020-06-07 15:11:23 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { Route, Switch } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+import rootReducer from './redux/rootReducer';
 import Layout from './components/Layout/Layout';
 import Channels from './components/Channels/Channels';
 import Channel from './components/Channel/Channel';
 
-
 function App() {
+  const store = createStore(rootReducer)
   return (
-    <HashRouter>
-      <Layout>
-        <Switch>
-          <Route path='/' component={Channels} exact />
-          <Route path='/channels' component={Channels} />
-          <Route path='/channel/:id/' component={Channel} />
-        </Switch>
-      </Layout>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <Layout>
+          <Switch>
+            <Route path='/' component={Channels} exact />
+            <Route path='/channels' component={Channels} />
+            <Route path='/channel/:id/' component={Channel} />
+          </Switch>
+        </Layout>
+      </HashRouter>
+    </Provider>
   );
 }
 
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+
+}
+
+export default function (state = initialState, action) {
+    console.log('reducer', action)
+
+    return state
+}
